ebf8582018f9df5b474b4cf3d1ad5a137ac44c28
Gabriella Cristini <126834537+CristiniGalvao@users.noreply.github.com>
2024-05-07 22:28:10 -0300

    add

---
+++ Clinica1/src/main/java/br/unipar/programacaointernet/clinica1/Clinica1Application.java
@@ -0,0 +1,15 @@
+package br.unipar.programacaointernet.clinica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Clinica1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Clinica1Application.class, args);
+    }
+
+}
---
+++ Clinica1/src/test/java/br/unipar/programacaointernet/clinica1/Clinica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unipar.programacaointernet.clinica1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clinica1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
